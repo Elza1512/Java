d1f69359bab9be2f7d5ab40c02df11aa5132f416
Elza1512 <109744471+Elza1512@users.noreply.github.com>
2022-12-22 18:11:40 +0400

    Lessjn

--- Lesson1.java
+++ Lesson1.java
@@ -1,2 +1,28 @@
 public class Lesson1 {
+    public static void main(String[] args) {
+        int a = 6;
+        double d = 5;
+        float f = 4.2f;
+        long l = 34;
+        short s = 2;
+        byte b = 12;
+        boolean bool = true;
+
+        char ch = 'p';
+        ch = 12;
+
+        String str = "Hello";
+        String str2 = new String("hbjhb");
+
+        int [] ints = new int[10];
+        ints[4] = ints[1] = 6;
+
+        if (a <= 9) {
+            a = 2;
+        } else if (a > 10) {
+            a = 10;
+        } else {
+            a = 0;
+        }
+    }
 }

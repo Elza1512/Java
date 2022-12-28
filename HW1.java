4d6dc45c9a2f520a8878c4a8217047385a2a3787
Elza1512 <109744471+Elza1512@users.noreply.github.com>
2022-12-28 18:25:12 +0400

    HW1

--- HW1.java
+++ HW1.java
@@ -1,6 +1,6 @@
 import java.util.Random;
 public class HW1 {
-    public static void main(String[] args) {
+    public static void rand(String[] args) {
         Random random = new Random();
         int[] ints = new int[1];
         for (int i = 0; i <= 0; i++) {
@@ -8,6 +8,34 @@ public class HW1 {
         }
         for (int i : ints) {
             System.out.println(i);
+        }
+    }
+
+    public static void bit(String[] args) {
+    }
+
+    int bit1(int n) {
+        int t = 1 << 30;
+        while (n < t) t >>= 1;
+        return t;
+    }
+    public static void max(String[] args) {
+        int[] ints = new int[1];
+        int max_s = Short.MAX_VALUE;
+        for (int j = i; j <= max_s; j++) {
+            if (j % n != 0) {
+                int m1 = j;
+            }
+
+        }
+    }
+    public static void min(String[] args) {
+        int[] ints = new int[1];
+        int min_s = Short.MIN_VALUE;
+        for (int j = i; j <= min_s; j++) {
+            if (j % n != 0) {
+                int m2 = j;
+            }
         }
     }
 }
